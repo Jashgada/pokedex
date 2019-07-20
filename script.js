d7f7513c65f7023698bc434f3e01aa3d7820cccf
Jashgada <jashgada93@gmail.com>
2019-07-20 16:45:46 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function onSubmit()
+{
+var pokemon = document.getElementById("Poke-name").value;
+console.log(pokemon)
+var request = new XMLHttpRequest();
+request.open("GET", "https://pokeapi.co/api/v2/pokemon/"+pokemon, true);
+request.onload = function()
+{
+    var data = JSON.parse(this.response);
+    console.log(data.types[0].type.name);
+}
+request.send();
+}
