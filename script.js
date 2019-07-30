996f9f4ffec74f511a7e210f186d965c7bf0af08
Jashgada <jashgada93@gmail.com>
2019-07-30 11:19:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,21 +5,19 @@ console.log(pokemon)
 var request = new XMLHttpRequest();
 request.open("GET", "https://pokeapi.co/api/v2/pokemon/"+pokemon, true);
 var pokedata;
+request.send();
 request.onload = function()
 {
     var data = JSON.parse(this.response);
+    dispStats(data);
     console.log(data.types[0].type.name);
     pokedata = data;
 }
-request.send();
-window.open("pokedex-page.html");
 }
 
 function dispStats(pokeObject)
-{
-    var x = {valu: 1, value: 2};
-    console.log(x.valu);
-    console.log(pokeObject);
-    document.getElementById("stat-para").innerHTML = "Type: " + x.valu; 
-    document.getElementById("stat-para").innerHTML = "Type: " + pokeObject.types[0].type.name;  
+ {   //document.getElementById("stat-para").innerHTML = "Type: " + x.valu;
+ window.location.href = "pokedex-page.html";    
+ document.getElementById("stat-para").innerHTML = "Type: "; 
+    
 }
